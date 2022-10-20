38f8275e01ce3a91fae24564775fa8ab59e93089
AidanSweet216 <77294982+AidanSweet216@users.noreply.github.com>
2022-10-20 09:54:30 -0600

    sprite stuff

--- Web Server/Sketch.js
+++ Web Server/Sketch.js
@@ -1,6 +1,9 @@
+//let s;
 function setup() {
 	createCanvas(800, 400);
-    
+  //   s = new Sprite();
+	// s.height = 50;
+	//s.width = 50;
 }
 
 
@@ -8,18 +11,21 @@ function draw() {
 	background(0,250,200); 
 	fill(0);
 	textAlign(CENTER);
-	text('Click to create a new sprite', width / 2, height / 2);
-
+	text('You are logged in!', width / 2, height / 2);
+	//group.draw();
 	
-	if (mouse.presses()) {
+	
+	//if(mouse.presses())  {
 		
-		let s = new Sprite(mouse.x, mouse.y, 30, 30);
+		let s = new Sprite(random(800), random(400), 30, 30);
 		
 		s.vel.x = random(-5, 5);
 		s.vel.y = random(-5, 5);
+		console.log(s.velocity.x +","+ s.velocity.y);
 		
-	}
-
+	//}
+	
+	
 }
 
 
